1b8e001e5995d38fa19c200c7b9a6b39bb1a4186
teekay <tk.kayas44@gmail.com>
2025-09-21 21:16:23 +0200

    interpolation and data binding

--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummyUser';
+
+const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
 
 @Component({
   selector: 'app-user',
   imports: [],
   templateUrl: './user.html',
-  styleUrl: './user.css'
+  styleUrls: ['./user.css'],
 })
 export class User {
-
+  selectedUser = DUMMY_USERS[randomIndex];
 }
